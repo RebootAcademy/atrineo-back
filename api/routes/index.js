32ee2829908871e4f2c571352c2fae0e63840ce1
G. Carolina Hernández <carolinahrndez3@gmail.com>
2024-01-16 15:39:15 +0000

    created data router

--- api/routes/index.js
+++ api/routes/index.js
@@ -9,9 +9,11 @@ const {
 const userRouter = require('./user.router.js'); 
 const organizationRouter = require('./organization.router.js'); 
 const authRouter = require('./auth.router.js'); 
+const dataRouter = require('./data.router.js'); 
 
 router.use('/users', userRouter)
 router.use('/organizations', organizationRouter)
 router.use('/auth', authRouter)
+router.use('/data', dataRouter)
 
 module.exports = router
