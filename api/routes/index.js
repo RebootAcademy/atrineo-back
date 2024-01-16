57a9b818071028e724435db8a73e7fcfc3732da0
Alber0087 <albertorg0087@gmail.com>
2024-01-16 15:26:24 +0000

    location, country, regions and districts routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -8,10 +8,18 @@ const {
 
 const userRouter = require('./user.router.js'); 
 const organizationRouter = require('./organization.router.js'); 
-const authRouter = require('./auth.router.js'); 
+const locationRouter = require('./location.router.js')
+const countryRouter = require('./country.router.js')
+const regionRouter = require('./region.router.js')
+const districtRouter = require('./district.router.js')
+const authRouter = require('./auth.router.js')
 
 router.use('/users', userRouter)
 router.use('/organizations', organizationRouter)
+router.use('/location', locationRouter)
+router.use('/country', countryRouter)
+router.use('/region', regionRouter)
+router.use('/district', districtRouter)
 router.use('/auth', authRouter)
 
 module.exports = router
