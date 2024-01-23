57694447b2fb9d588dd89f88d4059d835077d98c
Alber <albertorg0087@gmail.com>
2024-01-23 09:13:44 +0000

    Revert "Develop"

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ process.stdout.write('\x1B[2J\x1B[0f') // Clear terminal screen
 
 require('dotenv').config()
 
-const bodyParser = require('body-parser')
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
@@ -13,8 +12,7 @@ const dbConnect = require('./database')
 const app = express()
   .use(cors())
   .use(morgan('dev'))
-  .use(express.json({ limit: '10mb' }))
-  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
   .use('/api', require('./api/routes'))
   .listen(process.env.PORT, async (error) => {
     if (error) throw new Error (error)
