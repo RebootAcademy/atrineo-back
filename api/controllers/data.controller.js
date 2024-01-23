981ac561c522ceaf7520ee398e90b389f6e7f355
G. Carolina Hernández <carolinahrndez3@gmail.com>
2024-01-23 14:29:44 +0000

    create data controller

--- api/controllers/data.controller.js
+++ api/controllers/data.controller.js
@@ -1,4 +1,6 @@
 const Data = require('../models/data.model')
+const Division4 = require('../models/division4.model')
+const Location = require('../models/location.model')
 const { transformData } = require('../utils/transformData')
 
 // CREATE/POST - create a new data
@@ -6,15 +8,29 @@ const createData = async (req, res) => {
   try {
     const newDataArray = []
 
-    req.body.forEach(element => {
+    console.log
+
+    for (const element of req.body) {
       element.geometry = transformData(element.geometry)
+      const division4 = await Division4.findOne({ postalCode: { $in: element.districtId } });
+      const location = await Location.findOne({ division4 });
+
       const newData = new Data({
         ...element,
-        locationId: null
+        locationId: location._id
       })
+
       newData.save()
-      newDataArray.push(newData)
-    })
+      newDataArray.push({
+        _id: newData._id,
+        name: newData.name,
+        latitud: newData.latitude,
+        longitude: newData.longitude,
+        districtId: newData.districtId,
+        districtName: newData.districtName,
+        locationId: newData.locationId,
+      })
+    }
 
     return res.status(201).json({
       success: true,
