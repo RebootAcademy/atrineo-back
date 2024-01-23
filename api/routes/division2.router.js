76d792e21806d74e35d01bd68b1fe68d1706027e
G. Carolina Hernández <carolinahrndez3@gmail.com>
2024-01-23 16:25:05 +0000

    fix permissions

--- api/routes/division2.router.js
+++ api/routes/division2.router.js
@@ -1,5 +1,10 @@
 const router = require('express').Router()
 
+const { 
+  isAuth,
+  isWizard
+} = require('../middlewares')
+
 const {
   createDivision2,
   getAllDivision2,
@@ -9,10 +14,10 @@ const {
 } = require('../controllers/division2.controller')
 
 router
-  .post('/', createDivision2)
+  .post('/', isAuth, isWizard, createDivision2)
   .get('/', getAllDivision2)
   .get('/:id', getDivision2ById)
-  .patch('/:id', updateDivision2)
-  .delete('/:id', deleteDivision2)
+  .patch('/:id', isAuth, isWizard, updateDivision2)
+  .delete('/:id', isAuth, isWizard, deleteDivision2)
 
 module.exports = router
