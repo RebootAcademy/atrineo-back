9247643d79380068e6337e8149d5e3a9d797d650
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2024-03-05 16:22:22 +0000

    login - fixed response format

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -30,7 +30,7 @@ const login = async (req, res) => {
     return res.status(200).json({
       success: true,
       message: 'User logged in',
-      result: {token, userObject}
+      result: {token, user: userObject}
     })
 
   } catch (error) {
